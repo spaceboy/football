e97d1cb3751fcaf6904fe44a04e9f8a1e5fad817
Jiří Votoček <jiri.votocek@centrum.cz>
2022-06-26 17:23:54 +0200

    Download source preparation

---
+++ js/Download.js
@@ -0,0 +1,75 @@
+class Download {
+    header = {
+        "about": [
+            "====================================================================================",
+            "#   _____           _   _           _ _    ____                 _     _            #",
+            "#  |  ___|__   ___ | |_| |__   __ _| | |  / ___|_ __ __ _ _ __ | |__ (_) ___ ___   #",
+            "#  | |_ / _ \ / _ \| __| '_ \ / _` | | | | |  _| '__/ _` | '_ \| '_ \| |/ __/ __|  #",
+            "#  |  _| (_) | (_) | |_| |_) | (_| | | | | |_| | | | (_| | |_) | | | | | (__\__ \  #",
+            "#  |_|  \___/ \___/ \__|_.__/ \__,_|_|_|  \____|_|  \__,_| .__/|_| |_|_|\___|___/  #",
+            "#                                                        |_|                       #",
+            "====================================================================================",
+            " Football Graphics data file                   https://spaceboy.github.io/football/ ",
+            "                                       free to use FOOTBALL infograpshics generator ",
+            "",
+            " Warning: ",
+            " This is Football Graphics data file. ",
+            " Don't edit it, unless you are sure what are you doing. ",
+        ],
+        "application": {
+            "name": "Footbal Graphics",
+            "version": "0.1",
+            "url": "https://spaceboy.github.io/football/",
+            "author": "spaceboy",
+            "using": [
+                "This site uses:",
+                "- DOM to Image (https://github.com/tsayen/dom-to-image)",
+                "- Data-uri-to-img-url (http://aminariana.github.io/data-uri-to-img-url/), (https://github.com/aminariana/data-uri-to-img-url)",
+                "- Font awesome",
+                "- Google fonts",
+                "- Text to ASCII art generator (https://patorjk.com/software/taag/)",
+                "- And a few scripts by Spaceboy, of course ;-)"
+            ]
+        },
+        "data": {
+            "club-info": {
+                "name": "TJ Slavoj Houslice",
+                "chairman": "Václav Orel",
+                "manager": "Bohumír Zenkl",
+                "players": [
+                    {
+                        "number": 1,
+                        "name": "Vachopulos"
+                    },
+                    {
+                        "number": 2,
+                        "name": "Mažňák"
+                    },
+                    {
+                        "number": 3,
+                        "name": "Provazník"
+                    }
+                ]
+            },
+            "jersey-field": {
+                "shirtType": "stripesVertical",
+                "colorShirt1": "green",
+                "colorShirt2": "white",
+                "colorShorts": "darkgreen",
+                "colorBody": "antiquewhite"
+            },
+            "jersey-goalie": {
+                "shirtType": "compact",
+                "colorShirt1": "orange",
+                "colorShirt2": "white",
+                "colorShorts": "orange",
+                "colorBody": "antiquewhite"
+            },
+            "match-info": {
+                "type": "ligové utkání",
+                "partner": "FC Real Horní Lhota",
+                "place": "Dřevona arena Dolní Lhota"
+            }
+        }
+    }
+}
